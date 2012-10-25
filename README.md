3679a952f966dd2eacdaeb93e08be801a07b6984
萌名雪 <moe@nayuki.info>
2012-10-25 21:18:23 +0800

    扩充readme内容和js的注释

--- README.md
+++ README.md
@@ -1,9 +1,40 @@
 #CnBeta 自古CB出评论 sharing plugin based on jQuery.
 
-Any modify or suggestion is welcomed.
+###这是什么？
 
-Help yourself.
+一个使用twitter和新浪微博分享CB新闻评论的userscript插件。
 
-If you just want to install this script, [CLICK HERE](http://userscripts.org/scripts/show/131500).
+输出格式：`评论正文 —— 《新闻标题》 原帖地址 #自古CB出评论`
 
-Again, THANK U ALL for using and modifying this script!
+###我只是想安装它
+[CLICK HERE](http://userscripts.org/scripts/show/131500).
+
+###What's new
+
+- 1.400 [!] 评论开头的诡异空格现在去掉了。 （感谢 [@JaHIY](https://twitter.com/JaHIY)）
+- 1.350 [!] 修正了新浪微博链接和hashtag相连，从而导致网址不正确的问题。
+- 1.300 [!] 改进了显示效果 （依然感谢 [@imSwin](https://twitter.com/imSwin)）
+- 1.200 [+] 增加新浪微博的支持
+- 1.100 [+] 推文中附带原文地址 （感谢 [@imSwin](https://twitter.com/imSwin)）
+- 1.000 [!] 基本功能实现 
+
+###使用方法
+
+打开任何[CnBeta](http://www.cnbeta.com)的新闻，在热门评论区和所有评论区的每条评论后都会出现蓝色的`推特`和`微博`的链接。点击会在新窗口打开发推或发微博的页面。
+
+###已知问题
+暂无。
+
+###需要做的
+- 需要更友好的外观和发推、发微博页面。
+- 更多SNS（包括但不限于网易微博、腾讯微博、Facebook等）的支持。
+- 自定义API？
+- 自动检测字数，并根据超出多少智能选择精简方案。
+- 将新闻标题中出现的双书名号替换成单书名号，符合中文使用规范。
+- 简易的设置页面，提供以上功能的开关。
+
+###鸣谢
+- [@anamewing](https://twitter.com/anamewing) ，没有他的[这句话](https://twitter.com/#!/anamewing/status/189572342802944000)就没有这个插件的诞生。
+- [@Pstrey](https://twitter.com/Pstrey)，[@dalphyx](https://twitter.com/dalphyx) 。他们帮我打开了思路。
+
+在使用中有意见和建议，请找twitter [@bgm38](https://twitter.com/bgm38) 或 新浪微博 [@相沢名雪](http://weibo.com/moenayuki)。当然也欢迎您的任何提交和衍生。
--- script/cnbeta.js
+++ script/cnbeta.js
@@ -52,22 +52,22 @@ if(Browser.chrome || typeof unsafeWindow == 'undefined') {
 })();
 // All your GM code must be inside this function
 // 以下为正文
+// CB新闻的标题在#news_title里，评论在每一个dd.re_detail里
 function main($) {
     $(document).ready(function()
                       { //当文档载入完毕时运行
                           var url, comment, title, finale, oricomment, twitter_link, weibo_link;
-                        //  url = "&url=" + window.location.href();
                           title = $("h3#news_title").text();
                           $("dd.re_detail").each(function(){
                             comment = $(this).text();//获取评论
                             finale = comment + " ——《" + title + "》 " + window.location.href + " "; //拼合tweet正文的结果
                             finale = $.trim(finale);//除去原评论字段中多余的空格字符
-                            finale = encodeURI(finale);
+							//要在此插入长度针对finale的判断模块
+                            finale = encodeURI(finale);//编码
 
-                            weibo_link = '<a href="http://service.weibo.com/share/share.php?title=' + finale + escape(" #") +encodeURI("自古CB出评论") +escape("#")+ '" class="weitag" target="_blank" title="以 #自古CB出评论# 发表">#微</a><style>.weitag{padding:3px 5px 3px 5px;}a.weitag:link{color:red;font-weight:900;}a.weitag:hover{color:#707070;}a.weitag:visted{color:red;font-weight:900;}</style>'
-                            twitter_link = '    <a href="' + encodeURI('https://twitter.com/intent/tweet?hashtags=自古CB出评论&text=') + finale + '" class="twitag" data-lang="zh-cn" data-related="bgm38" target="_blank" title="以 #自古CB出评论 发表">#推</a><style>.twitag{padding:3px 5px 3px 5px;}a.twitag:link{color:blue;font-weight:900;}a.twitag:hover{color:#707070;}a.twitag:visted{background-color:blue;font-weight:900;}</style>';
-                            $(this).append(twitter_link);//在评论的末尾添加link
-                        //   $(this "[href]").css("display","block")
+                            weibo_link = '<a href="http://service.weibo.com/share/share.php?title=' + finale + escape(" #") +encodeURI("自古CB出评论") +escape("#")+ '" class="weitag" target="_blank" title="以 #自古CB出评论# 发表">#微博</a><style>.weitag{padding:3px 5px 3px 5px;}a.weitag:link{color:red;font-weight:900;}a.weitag:hover{color:#707070;}a.weitag:visted{color:red;font-weight:900;}</style>'
+                            twitter_link = '    <a href="' + encodeURI('https://twitter.com/intent/tweet?hashtags=自古CB出评论&text=') + finale + '" class="twitag" data-lang="zh-cn" data-related="bgm38" target="_blank" title="以 #自古CB出评论 发表">#推特</a><style>.twitag{padding:3px 5px 3px 5px;}a.twitag:link{color:blue;font-weight:900;}a.twitag:hover{color:#707070;}a.twitag:visted{background-color:blue;font-weight:900;}</style>';
+                            $(this).append(twitter_link);//在评论的末尾附上最终结果
                             $(this).append(weibo_link);
                           });
                       });
