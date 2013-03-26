f1ddd2fdf89d0b9b4195099dfd1c5b122a662feb
dalphyx <wjcbmk@gmail.com>
2013-03-26 17:03:19 +0800

    主函数文件描述更新

--- script/cnbeta.js
+++ script/cnbeta.js
@@ -2,7 +2,7 @@
 // @name       Nayuki's CnBeta #自古CB出评论 sharing plugin 
 // @namespace  http://nayuki.info/
 // @version    1.500
-// @description  使用twitter分享CB新闻的评论，格式：评论 —— 《新闻标题》#自古CB出评论
+// @description  使用twitter和新浪微博分享CB新闻的评论，格式：评论 —— 《新闻标题》#自古CB出评论
 // @match      http://*.cnbeta.com/*
 // @copyright  2012-2013, Moe Nayuki
 // ==/UserScript==
