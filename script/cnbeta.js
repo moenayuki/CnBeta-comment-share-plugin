b7abe261fcaf16a74920d55e0281f511403aff46
dalphyx <wjcbmk@gmail.com>
2013-03-26 17:10:07 +0800

    fix coding style

--- script/cnbeta.js
+++ script/cnbeta.js
@@ -81,24 +81,25 @@ function main( $ ) {
             return word.substring( 0, 1 ).replace( /《/, "〈" ) + word.substring( 1 ).replace( /》/, "〉" );
         });
 
-            $( "dd.re_detail" ).each(function() {
+        $( "dd.re_detail" ).each(function() {
 
-                comment = $(this).text(); //获取评论
+            comment = $(this).text(); //获取评论
 
-                //微博分享scheme://host:port/path?query#fragment这样的链接需要去除#fragment部分,否则会没有tag
-                            
-                finale = comment + " ——《" + finale_title + "》 " + window.location.href.replace( window.location.hash, '' ) + " "; //拼合tweet正文的结果
-                finale = $.trim( finale ); //除去原评论字段中多余的空格字符
+            //微博分享scheme://host:port/path?query#fragment这样的链接需要去除#fragment部分,否则会没有tag
+                        
+            finale = comment + " ——《" + finale_title + "》 " + window.location.href.replace( window.location.hash, '' ) + " "; //拼合tweet正文的结果
+            finale = $.trim( finale ); //除去原评论字段中多余的空格字符
 
-				//要在此插入长度针对finale的判断模块
-                finale = encodeURI( finale ); //编码
+            //要在此插入长度针对finale的判断模块
+            finale = encodeURI( finale ); //编码
 
-                weibo_link   = '<a href="http://service.weibo.com/share/share.php?title=' + finale + escape( " #" ) + encodeURI( "自古CB出评论" ) + escape( "#" )+ '" class="weitag" target="_blank" title="以 #自古CB出评论# 发表">#微博</a><style>.weitag{padding:3px 5px 3px 5px;}a.weitag:link{color:red;font-weight:900;}a.weitag:hover{color:#707070;}a.weitag:visted{color:red;font-weight:900;}</style>'
-                twitter_link = '    <a href="' + encodeURI( "https://twitter.com/intent/tweet?hashtags=自古CB出评论&text=" ) + finale + '" class="twitag" data-lang="zh-cn" data-related="bgm38" target="_blank" title="以 #自古CB出评论 发表">#推特</a><style>.twitag{padding:3px 5px 3px 5px;}a.twitag:link{color:blue;font-weight:900;}a.twitag:hover{color:#707070;}a.twitag:visted{background-color:blue;font-weight:900;}</style>';
-                
-                $(this).append( twitter_link );//在评论的末尾附上最终结果
-                $(this).append( weibo_link );
+            weibo_link = '<a href="http://service.weibo.com/share/share.php?title=' + finale + escape( " #" ) + encodeURI( "自古CB出评论" ) + escape( "#" ) + '" class="weitag" target="_blank" title="以 #自古CB出评论# 发表">#微博</a><style>.weitag{padding:3px 5px 3px 5px;}a.weitag:link{color:red;font-weight:900;}a.weitag:hover{color:#707070;}a.weitag:visted{color:red;font-weight:900;}</style>'
+            twitter_link = '    <a href="' + encodeURI( "https://twitter.com/intent/tweet?hashtags=自古CB出评论&text=" ) + finale + '" class="twitag" data-lang="zh-cn" data-related="bgm38" target="_blank" title="以 #自古CB出评论 发表">#推特</a><style>.twitag{padding:3px 5px 3px 5px;}a.twitag:link{color:blue;font-weight:900;}a.twitag:hover{color:#707070;}a.twitag:visted{background-color:blue;font-weight:900;}</style>';
+            
+            $(this).append( twitter_link ); //在评论的末尾附上最终结果
+            $(this).append( weibo_link );
 
-            });
+        });
+
     });
 };
